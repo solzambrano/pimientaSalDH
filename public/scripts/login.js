66caff527ac779cd480bc02a5c89fc5797785973
solzambrano <solzambrano.8@gmail.com>
2024-07-01 19:58:07 -0300

    display submenu user

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,3 +1,8 @@
-document.getElementById.addEventListener('click',()=>{
+let userMenu=document.getElementById('user-menu');
+let user=document.getElementById('user');
+
+user.addEventListener('click',(e)=>{
     console.log('hola')
+    e.stopPropagation();
+  userMenu.style.display = userMenu.style.display === 'block' ? 'none' : 'block';
 })
