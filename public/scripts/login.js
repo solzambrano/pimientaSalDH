878d74f9fd8844cfe062b1949394a8d5fe60e27e
solzambrano <solzambrano.8@gmail.com>
2024-07-01 21:12:09 -0300

    fix script nav - mostrar opciones detalle menu

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -2,6 +2,7 @@ let userMenu=document.getElementById('user-menu');
 let user=document.getElementById('user');
 
 user.addEventListener('click',(e)=>{
+  console.log('estoy en el script');
     e.stopPropagation();
   userMenu.style.display = userMenu.style.display === 'block' ? 'none' : 'block';
 })
